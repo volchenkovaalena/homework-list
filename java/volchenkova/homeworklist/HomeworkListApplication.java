7e39e847ce71bec2f29df6659a25f54eb2f438aa
alena <misamistake@>
2024-03-20 09:57:32 +0300

    Initial commit

---
+++ java/volchenkova/homeworklist/HomeworkListApplication.java
@@ -0,0 +1,13 @@
+package volchenkova.homeworklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkListApplication.class, args);
+	}
+
+}
